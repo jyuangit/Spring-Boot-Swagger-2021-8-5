638344a47b98bcab1d187b0628c89c9b8e4d8c48
jyuangit <jiyuan233@qq.com>
2021-08-05 11:18:42 +0800

    first commit

--- src/main/java/com/example/SpringBootRestful/controller/UserController.java
+++ src/main/java/com/example/SpringBootRestful/controller/UserController.java
@@ -3,13 +3,17 @@ package com.example.SpringBootRestful.controller;
 import com.example.SpringBootRestful.entity.User;
 import com.example.SpringBootRestful.exception.BusinessException;
 import com.example.SpringBootRestful.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 // Todo:此处写注解
-
+//@Api(value = "users", description = "Endpoint for user management")
+@EnableSwagger2
 @RestController
 @RequestMapping("/api")
 public class UserController {
@@ -18,23 +22,27 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/users")
+//    @ApiOperation(value = "getAllUser")
     public ResponseEntity getAllUser() {
         return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
     }
 
     @PostMapping("/users")
+//    @ApiOperation(value = "addUser")
     public ResponseEntity addUser(@RequestBody User user) {
         userService.addUser(user);
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/users/{userId}")
+//    @ApiOperation("updateUserById")
     public ResponseEntity updateUserById(@PathVariable int userId, @RequestBody User user) throws BusinessException {
         userService.updateUserById(userId, user);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/users/{userId}")
+//    @ApiOperation("deleteUserById")
     public ResponseEntity deleteUserById(@PathVariable int userId) throws BusinessException {
         userService.deleteUserById(userId);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
